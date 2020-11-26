d04ddeff6e0ea1c6b6890f1d60e347f197e87697
Arturo Fernández <arturo.developer@gmail.com>
2020-11-26 23:48:24 +0100

    Añadidos todos los cambios en la ROM y RAM, falta arreglar assert tb_RAM

--- dmem.sv
+++ dmem.sv
@@ -15,21 +15,21 @@
  *  mem0_dr - Read data output.
 */
 
-module dmem #(parameter DATA_WIDTH = 32, parameter MEM_DEPTH = 1024) (clk, mem0_dw, daddr_r, daddr_w, mem0_ena, mem0_rw, mem0_dr);
+module dmem #(parameter DATA_WIDTH = 32, parameter MEM_DEPTH = 1024) (clk, write_data, addr, mem_write, dout);
     input clk;
-    input [DATA_WIDTH-1:0]  mem0_dw; 
-    input [$clog2(MEM_DEPTH)-1:0] daddr_r, daddr_w; 
-    input mem0_ena, mem0_rw; 
-    output [DATA_WIDTH-1:0] mem0_dr; 
+    input [DATA_WIDTH-1:0]  write_data; 
+    input [$clog2(MEM_DEPTH)-1:0] addr; 
+    input mem_write; 
+    output [DATA_WIDTH-1:0] dout; 
 
     logic [DATA_WIDTH-1:0] DMEM [0:MEM_DEPTH-1]; //packed and unpacked array
 
     // Synchronous Write:
     always_ff @(posedge clk) begin
-    if (mem0_ena && mem0_rw == 1'b1) 
-            DMEM[daddr_w]<=mem0_dw;
+    if (mem_write == 1'b1) 
+            DMEM[addr]<=write_data;
     end
 
     // Asynchronous Read:
-    assign mem0_dr =  (mem0_rw==1'b0 && mem0_ena) ? DMEM[daddr_r]:1'b0;
+    assign dout = DMEM[addr];
 endmodule
