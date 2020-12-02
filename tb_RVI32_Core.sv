b7d45fc23b17c68657cf3481050950c74936bf5d
Sergio Micó <sermiro@teleco.upv.es>
2020-12-02 13:09:47 +0100

    Cambios interfaz y tb
    Añadidos el core pasado por interfaz y su instanciación en el tb

--- tb_RVI32_Core.sv
+++ tb_RVI32_Core.sv
@@ -6,7 +6,12 @@ module tb_RVI32_Core;
     reg [31:0] idata, ddata_r; 
     wire [31:0] iaddr, daddr, ddata_w, d_rw; 
 
-    RVI32_Core myCorason (.*);
+    // instanciación de la interfaz
+    IF interfaz (.CLK(CLK), .RESET_N(RESET_N));
+
+    // instanciación del core con interfaz
+    // RVI32_Core myCorason (.*);
+    duv myCorason (.BUS(interfaz));
 
     always begin
         #(T/2) CLK <= ~CLK;
