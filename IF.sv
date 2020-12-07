f246818c4f67492c3d196e8da535bc0e65611253
Arturo Fernández <arturo.developer@gmail.com>
2020-12-08 00:23:03 +0100

    Interfaz refactorizada

--- IF.sv
+++ IF.sv
@@ -1,5 +1,13 @@
 `timescale 1ns/1ps 
-
+/*
+ * Interface: IF
+ *    DUT input data class.
+ *
+ * Parameters:
+ *
+ *  WIDTH - Address Width 
+ *  
+*/
 interface IF #(parameter DATA_WIDTH = 32, parameter MEM_DEPTH = 1024) (
     input CLK, RESET_N,
     ref logic [31:0] Regs [31:0],
@@ -7,18 +15,8 @@ interface IF #(parameter DATA_WIDTH = 32, parameter MEM_DEPTH = 1024) (
     ref logic [31:0] imm, idata, ddata_r, iaddr, daddr, ddata_w,
     ref logic d_rw 
 );
-
-    /*!< CLOCKING BLOCK DEFINITIONS: */
-    // clocking cb_stimulus @(posedge CLK); //Anyadir descripcion
-    //     default input #1ns output #1ns; /*!< input and output skews sample */
-    //     input #1ns S;
-    //     input #1ns END_MULT;
-    //     output #1ns START;
-    //     output #1ns A;
-    //     output #1ns B;
-    // endclocking:cb_stimulus
-    
-    clocking cb_monitor @(negedge CLK); //Anyadir descripcion
+   
+    clocking cb_monitor @(negedge CLK); 
         default input #1ns output #1ns; /*!< input and output skews sample */
         input #1ns idata;  
         input #1ns ddata_r; 
@@ -33,23 +31,4 @@ interface IF #(parameter DATA_WIDTH = 32, parameter MEM_DEPTH = 1024) (
 
     /*!< INTERFACE MODPORT DEFINITIONS: */
     modport monitor (clocking cb_monitor, input CLK);
-    // modport mem (
-    //     input CLK, 
-    //     input iaddr,
-    //     input daddr,
-    //     input ddata_w,
-    //     input d_rw,       
-    //     output idata,
-    //     output ddata_r
-    // );
-    // modport core (
-    //     input CLK, 
-    //     input RESET_N,
-    //     input idata,
-    //     input ddata_r,
-    //     output iaddr,
-    //     output daddr,
-    //     output ddata_w,
-    //     output d_rw
-    // );
 endinterface
